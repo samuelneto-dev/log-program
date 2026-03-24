9c59642e4c86ef9c75f5ea4b5713b5f94fcf7b52
samuelneto-dev <samuel.s.neto0dev@gmail.com>
2026-03-23 21:25:36 -0300

    Atividade de vetores
    Atividade de vetores 23/03/2026

---
+++ vetor.js
@@ -0,0 +1,9 @@
+let numero = [7 ,8 ,9]
+
+if(numero[0] >7 && numero[1] >7 && numero[2] >7){
+    console.log("Aprovado direto")
+}else if(numero[0] < 4 || numero[1] <4 || numero[2] <4){
+    console.log("Reprovado") 
+}else{
+    console.log("Recuperação")
+}
