61c2d688d3afbed7dd06bd9018e534ce6b443f48
samuelneto-dev <samuel.s.neto0dev@gmail.com>
2026-03-25 00:12:13 -0300

    Alteração da questão 6

--- atividade/atv_validacao_senha6.js
+++ atividade/atv_validacao_senha6.js
@@ -1,8 +1,8 @@
-let senha = "1234567"
+let senha = "45645678"
 
 if (senha.length < 8) {
     console.log("Senha invalida, no minimo 8 caracteres.")
-}else if (senha !== "123") {
+}else if (senha.includes("123")) {
     console.log("Senha inválida, a senha não deve ter os caracteres '123'.")
 }else {
     console.log("Senha válida, a senha atende aos requisitos.")
