01bb27740cf18b61ac558a3a2b34f0a722284f20
samuelneto-dev <samuel.s.neto0dev@gmail.com>
2026-03-25 00:03:14 -0300

    Lista de atividade

---
+++ atividade/atv_validacao_senha6.js
@@ -0,0 +1,9 @@
+let senha = "1234567"
+
+if (senha.length < 8) {
+    console.log("Senha invalida, no minimo 8 caracteres.")
+}else if (senha !== "123") {
+    console.log("Senha inválida, a senha não deve ter os caracteres '123'.")
+}else {
+    console.log("Senha válida, a senha atende aos requisitos.")
+}
